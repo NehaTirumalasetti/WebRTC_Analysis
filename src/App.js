fee5de62132380d90bcd38e866ab929cf773a996
NehaTirumalasetti <ntirumalasetti@gmail.com>
2025-03-08 01:31:23 -0800

    Setting up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect, useRef } from 'react';
+import webrtcService from './webrtcService';
 
 function App() {
+  const [option, setOption] = useState(null);
+  const [sdpOffer, setsdpOffer] = useState(null);
+  const [sdpAnswer, setsdpAnswer] = useState('');
+  const user1 = useRef(null);
+  const user2 = useRef(null);
+  let localStream = null;
+
+  useEffect(() => {
+   const initStream =  async() => {
+      try{
+       localStream = await webrtcService.init();
+    } catch (error){
+      console.error(error);
+    }
+  }
+  initStream();
+  }, []);
+
+  function onGenerateSDP (){
+    setOption('caller');
+    webrtcService.createOffer(setsdpOffer).then((offer) => {
+      setsdpOffer(offer[0]);
+      // user2.current.srcObject = offer[1];
+    }).catch(console.error);
+    // user1.current.srcObject = localStream;
+  }
+  
+  function onAcceptSDP(){
+    setOption('receiver');
+    // user2.current.srcObject = localStream;
+  }
+  function handleAnswer(){
+    webrtcService.handleAnswer(sdpAnswer).then(() => {
+      console.log('answer handled');
+    }).catch(console.error);
+  }
+
+  function handleOffer(){
+    webrtcService.createAnswer(sdpOffer, setsdpAnswer).then((answer) => {
+      setsdpAnswer(answer[0]);
+      // user1.current.srcObject = answer[1];
+    }).catch(console.error);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>WebRTC Project</h3>
+      <div className="Video">
+        <video className="video-player"  id="user-1" autoPlay playsInline></video>
+        <video className="video-player"  id="user-2" autoPlay playsInline></video>
+      </div>
+      {!option &&  <ButtonBar onGenerateSDP={onGenerateSDP} onAcceptSDP={onAcceptSDP}></ButtonBar>}
+      {option==='caller' && <MessageBubble displayText={sdpOffer} title={'SDP Offer'}/>}
+      {option==='caller' && sdpOffer!==null && <InputBubble title={'answer'} onClick={handleAnswer} onChange={setsdpAnswer}/>}
+      {option==='receiver' &&<InputBubble title={'offer'} onClick={handleOffer} onChange={setsdpOffer}/>}
+      {option==='receiver'&& sdpAnswer!==null && <MessageBubble displayText={sdpAnswer} title={'SDP Answer'}/>}
     </div>
   );
+}
+
+function ButtonBar({ onGenerateSDP, onAcceptSDP }) {
+  return(     
+  <div className="buttonBar">
+    <Button className = "inv" displayText= "Invite User" onClick = {onGenerateSDP} ></Button>
+    <Button className = "acp" displayText= "Accept Invite" onClick = {onAcceptSDP} ></Button>
+  </div>
+  )
+}
+
+function Button({className, displayText, onClick}) {
+  return(
+    <button className={`but ${className}`}onClick={onClick}>{displayText}</button>
+  )
+}
+
+function MessageBubble({displayText, title}) {
+  return (
+    <div className="msg">
+      <p className="grey">
+        <h3>{title}</h3>
+          {displayText}
+      </p> 
+    </div>
+  )
+}
+
+function InputBubble({title, onClick, onChange}) {
+  return (
+    <div>
+      <input type="text" className="ip" placeholder={`Paste SDP ${title}`} onChange={(value) => onChange(value.target.value)}/>
+      <button className ="but acp" onClick={onClick} >Paste SDP {title}</button>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/webrtcService.js
@@ -0,0 +1,85 @@
+let localStream, remoteStream, peerConnection;
+
+const configuration = {
+    iceServers: [
+    {
+        urls:['stun:stun1.1.google.com:19302', 'stun:stun2.1.google.com:19302']
+    },
+    ]
+};
+const webrtcService = {
+async init() {
+    localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false});
+    document.getElementById('user-1').srcObject = localStream;
+    return localStream
+},
+
+async createOffer(setsdpOffer){
+    peerConnection = new RTCPeerConnection(configuration);
+
+    localStream.getTracks().forEach((track) => {
+        peerConnection.addTrack(track, localStream);
+    })
+
+    remoteStream = new MediaStream();
+    document.getElementById('user-2').srcObject = remoteStream;
+
+    peerConnection.ontrack = async (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+            console.log('track')
+            remoteStream.addTrack(track)
+        })
+    }
+
+    let offer = await peerConnection.createOffer();
+
+    peerConnection.onicecandidate = async (event) => {
+        if(event.candidate){
+            setsdpOffer = JSON.stringify(peerConnection.localDescription);
+        }
+    }
+
+    await peerConnection.setLocalDescription(offer);
+    return([JSON.stringify(offer), remoteStream])
+},
+
+async createAnswer(sdpOffer, setsdpAnswer){
+    peerConnection = new RTCPeerConnection(configuration);
+
+
+    localStream.getTracks().forEach((track) => {
+        peerConnection.addTrack(track, localStream);
+    })
+
+    remoteStream = new MediaStream();
+    document.getElementById('user-2').srcObject = remoteStream;
+
+    peerConnection.ontrack = async (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+            console.log('track')
+            remoteStream.addTrack(track)
+        })
+    }
+
+    await peerConnection.setRemoteDescription(JSON.parse(sdpOffer));
+
+    let answer = await peerConnection.createAnswer();
+
+    peerConnection.onicecandidate = async (event) => {
+        if(event.candidate){
+            setsdpAnswer(JSON.stringify(peerConnection.localDescription));
+        }
+    }
+
+    // await peerConnection.setRemoteDescription(JSON.parse(sdpOffer));
+
+    await peerConnection.setLocalDescription(answer);
+    return[JSON.stringify(answer), remoteStream];
+},
+
+async handleAnswer(sdpAnswer){
+    await peerConnection.setRemoteDescription(JSON.parse(sdpAnswer));
+}
+};
+
+export default webrtcService;
